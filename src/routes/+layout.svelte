7a1edbf1679133ae2553c7317bc2c0c5d973914f
Max <7087907+maxpromer@users.noreply.github.com>
2026-05-06 17:12:09 +0700

    Fixed PWA build fail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import './layout.css';
+	import { assets } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.png" type="image/png" />
+	<link rel="icon" href="{assets}/favicon.png" type="image/png" />
 	<title>FlowCode</title>
 </svelte:head>
 {@render children()}
